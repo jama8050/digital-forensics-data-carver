b292d9235b3aa4c5cefe6a90a65ab2cc85b60a5c
Jacob Malcy <jama8050@colorado.edu>
2019-10-20 19:23:04 -0600

    Add file markers and basic main

--- carver.py
+++ carver.py
@@ -6,3 +6,20 @@ from hashlib import md5
 from _md5 import md5
 import os
 import sys
+
+FILE_MARKERS = {'jpg': (b'\xFF\xD8', b'\xFF\xD9'),
+                'png': (b'\x50\x4E\x47', b'\x49\x45\x4E\x44'),
+                'pdf_markers': (b'\x25\x50\x44\x46', b'\x25\x25\x45\x4F\x46')}
+directory_name = "./Malcy"
+
+
+def main():
+    # Create the folder named "Malcy" if it doesn't exist
+    if os.path.isdir(directory_name) is False:
+        os.mkdir(directory_name)
+
+    # TODO: Parse cmdline arguments
+
+
+if __name__ == "__main__":
+    main()
